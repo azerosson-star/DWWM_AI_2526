077c8f31444b3fe5efbc0bc4a5912fec1e65ce35
Patrick-Croquet <58164643+Patrick-Croquet@users.noreply.github.com>
2025-12-03 10:55:49 +0100

    Delete Back/MVC directory

--- Back/MVC/app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-from app.controllers import main_controller
-
-app.register_blueprint(main_controller.bp)
--- Back/MVC/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    @staticmethod
-    def get_database_uri():
-        return f"mysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
